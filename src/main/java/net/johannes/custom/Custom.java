4941355aa506c825800c1b5a29c5404c7ce777b3
johannes-wagmann <johannes.wagmann@gmail.com>
2022-05-09 09:54:06 +0200

    my project

---
+++ src/main/java/net/johannes/custom/Custom.java
@@ -0,0 +1,47 @@
+package net.johannes.custom;
+
+import com.mojang.logging.LogUtils;
+import net.johannes.custom.item.ModItems;
+import net.johannes.custom.world.structure.ModStructures;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import software.bernie.geckolib3.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Custom.MOD_ID)
+public class Custom
+{
+    public static final String MOD_ID = "custom";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public Custom()
+    {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModStructures.register(eventBus);
+
+        GeckoLib.initialize();
+
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/net/johannes/custom/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.johannes.custom.item;
+
+import net.johannes.custom.Custom;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Custom.MOD_ID);
+
+    public static final RegistryObject<Item> EMERALD_SWORD = ITEMS.register("emerald_sword",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
